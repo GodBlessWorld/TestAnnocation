b93886cd3099e111236da4da5832a78143406416
chelder86 <helder1986@gmail.com>
2013-01-23 21:15:59 +0100

    Basic files to generate everything with GMF.

--- Eugenia_ArcadeTongame.diagram/src/ArcadeTongame_editor/diagram/edit/helpers/GameEditHelper.java
+++
@@ -1,9 +0,0 @@
-package ArcadeTongame_editor.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class GameEditHelper
-		extends
-		ArcadeTongame_editor.diagram.edit.helpers.ArcadeTongame_editorBaseEditHelper {
-}
